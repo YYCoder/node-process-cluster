0efa49d52a27c37ecdc45b31767f4a6022a04e07
yuanye.markey <yuanye.markey@bytedance.com>
2019-11-15 14:42:46 +0800

    ~~~

--- cluster/basic/worker.js
+++ cluster/basic/worker.js
@@ -1,10 +1,10 @@
 const { worker } = require('cluster');
 const http = require('http');
 
-
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     console.log(worker.process.pid + ' 响应请求');
     res.end('hello');
-}).listen(5000, () => {
+}).listen(5000, '127.0.0.1', () => {
     console.log('process %s started', worker.process.pid);
+    // console.log(server.address().port);
 });
